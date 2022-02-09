8a69046224ac755ade4fe93538b8ed26f250a4d0
Fakhri-P127 <fakhrita@code.edu.az>
2022-02-10 02:10:27 +0400

    modified Task-1 folder

--- Task-1/classwork/Program.cs
+++ Task-1/classwork/Program.cs
@@ -7,30 +7,35 @@ namespace classwork
         static void Main(string[] args)
         {
             int number = Convert.ToInt32(Console.ReadLine());
-            int[] divisible= new int[number]; //number yazmaqdan basqa bir sey aglima gelmedi
+            int[] divisible; 
             int m = 0;
+
             if (number < 50)
+                
             {
+                divisible = new int[number / 3];
                 for (int i = 3; i <= number; i++)
                 {
                     if (i % 3 == 0)
-                    {
-                        if (m <= number/3)
+                    {          
+                        if (m <= number / 3)
                         {
                             divisible[m] = i;
                             Console.WriteLine(divisible[m]);
-                            m++;
-                        }                                              
+                            m++;               
+                        }             
                     }
+                   
                 }             
             }
 
             else if (number > 49 && number < 101)
             {
+                divisible = new int[number / 5];
                 for (int i = 5; i <= number; i++)
                 {
                     if (i % 5 == 0)
-                    {
+                    {            
                         if (m <= number/5)
                         {
                             divisible[m] = i;
@@ -43,10 +48,11 @@ namespace classwork
 
             else
             {
+                divisible = new int[number / 8];
                 for (int i = 8; i <= number; i++)
                 {
                     if (i % 8 == 0)
-                    {
+                    {                      
                         if (m <= number/8)
                         {
                             divisible[m] = i;
