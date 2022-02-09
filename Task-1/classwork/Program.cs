287e33ed6c39a9c751f26ce9b82250d117eb1a7d
Fakhri-P127 <fakhrita@code.edu.az>
2022-02-10 00:03:16 +0400

    added Daily-Tasks-1 folder

---
+++ Task-1/classwork/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace classwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            int[] divisible= new int[1];
+
+            if (number < 50)
+            {
+                for (int i = 3; i <= number; i++)
+                {
+                    if (i % 3 == 0)
+                    {
+                        for (int m = 0; m <= divisible.Length; m++)
+                        {                          
+                            divisible[m] = i;
+                            break;                          
+                        }
+                        
+                        foreach (int nums in divisible)
+                        {
+                            Console.WriteLine(nums);
+                        }
+                    }
+                }             
+            }
+
+            else if (number > 49 && number < 101)
+            {
+                for (int i = 5; i <= number; i++)
+                {
+                    if (i % 5 == 0)
+                    {
+                        for (int m = 0; m <= divisible.Length; m++)
+                        {
+                            divisible[m] = i;
+                            break;
+                        }
+
+                        foreach (int nums in divisible)
+                        {
+                            Console.WriteLine(nums);
+                        }
+                    }
+                }
+            }
+
+            else
+            {
+                for (int i = 8; i <= number; i++)
+                {
+                    if (i % 8 == 0)
+                    {
+                        for (int m = 0; m <= divisible.Length; m++)
+                        {
+                            divisible[m] = i;
+                            break;
+                        }
+
+                        foreach (int nums in divisible)
+                        {
+                            Console.WriteLine(nums);
+                        }
+                    }
+                }
+            }            
+        }
+    }
+}
---
+++ Task-2/Task-2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] animals = {"Seal","Duck","Cat","Snake","Elephant","Crocodile","Dog" };
+            foreach (string animal in animals)
+            {
+                if (animal.Length > 5)
+                {
+                    Console.WriteLine(animal);
+                }
+            }
+        }
+    }
+}
